48c29f60302d7b5222cf040c0c5982783d4a9f3e
Marinellys Figueroa <marinellys.fgueroa@gmail.com>
2018-08-23 13:39:45 -0500

    Suprimiendo Archivos Duplicados

--- backend/db/migrate/20180705023238_create_institutions.rb
+++
@@ -1,9 +0,0 @@
-class CreateInstitutions < ActiveRecord::Migration[5.2]
-  def change
-    create_table :institutions do |t|
-      t.text :description
-
-      t.timestamps
-    end
-  end
-end
--- backend/test/models/tipo_documento_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TipoDocumentoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
